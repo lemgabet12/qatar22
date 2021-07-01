b9cae7335cf636a54dab6aff3d6c81c45a5ef32a
almaGeek <elghoul.alma@gmail.com>
2021-07-02 00:23:57 +0200

    appbar + drawer

--- lib/screens/components/my-app-bar.dart
+++ lib/screens/components/my-app-bar.dart
@@ -1,18 +1,27 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class Myappbar extends StatelessWidget implements PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      title: Text(
-        'FIFA QATAR 2022',
-        style: TextStyle(color: Colors.white),
-      ),
-      backgroundColor: Colors.red.shade900,
-    );
+        centerTitle: true,
+        iconTheme: IconThemeData(color: Colors.white),
+        title: SvgPicture.asset("images/logo2.svg",
+            width: 150.0, height: 100.0, fit: BoxFit.cover),
+        backgroundColor: Color.fromRGBO(153, 35, 43, 1.0),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.settings, color: Colors.white),
+            alignment: Alignment.centerLeft,
+            onPressed: () {
+              // do something
+            },
+          )
+        ]);
   }
 
   @override
-  Size get preferredSize => Size.fromHeight(60.0);
+  Size get preferredSize => Size.fromHeight(70.0);
 }
--- lib/screens/home/home-screen.dart
+++ lib/screens/home/home-screen.dart
@@ -6,6 +6,7 @@ import 'package:qatar22/screens/fan/fan-screen.dart';
 import 'package:qatar22/screens/feed/feed-screen.dart';
 import 'package:qatar22/screens/infos/infos-screen.dart';
 import 'package:qatar22/screens/program/program-screen.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -31,6 +32,41 @@ class HomeScreenState extends State<HomeScreen> {
           getCurrentPage(_selectedIndex),
         ],
       ),
+      drawer: Drawer(
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the drawer if there isn't enough vertical
+        // space to fit everything.
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Color.fromRGBO(153, 35, 43, 1.0),
+                ),
+                child: SvgPicture.asset("images/logo2.svg",
+                    width: 50.0, height: 50.0, fit: BoxFit.fitWidth)),
+            ListTile(
+              title: Text('Item 1'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text('Item 2'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
       bottomNavigationBar: CustumBottomNavigationBar(onSelectItem: (index) {
         _selectedIndex = index;
         setState(() {});
@@ -39,7 +75,6 @@ class HomeScreenState extends State<HomeScreen> {
   }
 
   getCurrentPage(int selectedIndex) {
-    print(selectedIndex);
     return _widgetOptions.elementAt(selectedIndex);
   }
 }
